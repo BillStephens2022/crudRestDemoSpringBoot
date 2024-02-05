ccecebf4991d31761d890260bb20f996cde987b1
BillStephens2022 <two4onebill@yahoo.com>
2024-02-05 07:42:37 -0500

    updated some configs

--- src/main/java/com/billstephens/crudRestDemo/dao/EmployeeRepository.java
+++ src/main/java/com/billstephens/crudRestDemo/dao/EmployeeRepository.java
@@ -2,7 +2,12 @@ package com.billstephens.crudRestDemo.dao;
 
 import com.billstephens.crudRestDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
+// the above annotation can be used to customize the endpoint .. (i.e. the default will be
+// to pluralize Employee and the endpoint will be "/employees".  If we wanted to customize to
+// "/members", we could use the above annotation to override the default behavior.
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it, no need to add any additional code in here
 }
